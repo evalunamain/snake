4d72699c38e5649875788714511c8514e7f8fc8a
Eva & Connie <eva+connie@appacademy.io>
2014-12-17 17:36:49 -0500

    Creates snake game with complete, but basic, functionality

---
+++ js/snake-view.js
@@ -0,0 +1,127 @@
+(function () {
+  if (typeof SnakeGame === "undefined"){
+    window.SnakeGame = {};
+  }
+
+  var View = SnakeGame.View = function($el) {
+    this.over = false;
+    this.pause = false;
+    this.canvas = $el;
+    this.board = new SnakeGame.Board();
+    this.generateApples();
+    this.startIntervals();
+  };
+
+  View.prototype.bindKeys = function(){
+    var board = this.board;
+    var view = this;
+
+    $(window).on('keydown', function(event){
+      if (event.which === 38) { // up
+        board.snake.turn("N");
+      } else if (event.which === 39) { // right
+        console.log("you pressed right");
+        board.snake.turn("E");
+      } else if (event.which === 40) { // down
+        board.snake.turn("S");
+      } else if (event.which === 37) { // left
+        board.snake.turn("W");
+      }  else if (event.which === 32) {
+        if (view.pause) {
+          view.startIntervals();
+        } else {
+          view.stopIntervals();
+        }
+      }
+    });
+
+  };
+
+  View.prototype.generateApples = function () {
+    var board = this.board;
+    var numApples = Math.floor(Math.random() * SnakeGame.Board.SIZE) + 1 ;
+    console.log(numApples);
+
+    board.apples = [];
+
+    for (var i = 0; i < numApples ; i++) {
+      var apple1 = Math.random() * SnakeGame.Board.SIZE
+      var apple2 = Math.random() * SnakeGame.Board.SIZE
+      var apple = [Math.floor(apple1), Math.floor(apple2)];
+      board.apples.push(apple);
+    }
+  };
+
+  View.prototype.startIntervals = function () {
+    this.pause = false;
+
+    if (!this.over) {
+      SnakeGame.interval1 = window.setInterval(function(){
+        this.step();
+      }.bind(this), 300);
+
+      SnakeGame.interval2 = window.setInterval(function(){
+        this.generateApples();
+      }.bind(this), 5000);
+    }
+  };
+
+  View.prototype.stopIntervals = function () {
+    this.pause = true;
+    window.clearInterval(SnakeGame.interval1);
+    window.clearInterval(SnakeGame.interval2);
+  };
+
+  // call snake move and redraw whole board
+  View.prototype.step = function(){
+    this.setUpBoard();
+    this.render();
+    this.bindKeys();
+    try {
+      this.board.snake.move();
+    } catch (e) {
+      alert(e.msg);
+      this.stopIntervals();
+      this.over = true;
+    }
+
+  };
+
+  View.prototype.render = function(){
+    var $lis = $("li");
+    var board = this.board;
+
+    $.each($lis, function (i, li) {
+      var $li = $(li);
+      var pos = $li.data("id");
+
+      $.each(board.snake.segments, function(i, segment) {
+        if (pos[0] === segment[0] && pos[1] === segment[1]) {
+          $li.toggleClass("snake");
+        }
+      });
+
+      $.each(board.apples, function(i, segment) {
+        if (pos[0] === segment[0] && pos[1] === segment[1]) {
+          $li.toggleClass("apple");
+        }
+      });
+
+    });
+  };
+
+  View.prototype.setUpBoard = function(){
+    this.canvas.empty();
+
+    for (var i = 0; i < SnakeGame.Board.SIZE; i++) {
+      var $ul = $("<ul class='group'></ul");
+      for (var j = 0; j < SnakeGame.Board.SIZE; j++) {
+        var $li = $("<li></li>");
+        $li.data("id", [i,j]);
+        $ul.append($li);
+      }
+      this.canvas.append($ul);
+    }
+  };
+
+})();
---
+++ js/snake.js
@@ -0,0 +1,88 @@
+(function(){
+  if (typeof SnakeGame === "undefined"){
+    window.SnakeGame = {};
+  }
+
+  var Snake = SnakeGame.Snake = function(options){
+    this.dir = options.dir;
+    this.segments = options.segments; // array of grid coords
+    this.board = options.board;
+    };
+
+  Snake.prototype.move = function(){
+    var snake = this;
+
+    var newSegment = [this.segments[0][0], this.segments[0][1]];
+
+    var incr = Snake.DIRS[snake.dir];
+    newSegment[0] += incr[0];
+    newSegment[1] += incr[1];
+
+    this.segments.pop();
+    this.segments.unshift(newSegment);
+    this.board.anyCollisions();
+  };
+
+
+  Snake.prototype.turn = function(newDir){
+    this.dir = newDir;
+  };
+
+  Snake.DIRS = {"N" : [0,-1], "E" : [1,0], "S" : [0,1], "W" : [-1, 0]};
+
+
+  var Board = SnakeGame.Board = function(){
+
+    var options = {
+      dir: "N",
+      segments: [[Math.floor(Board.SIZE/2), Math.floor(Board.SIZE/2)],
+      [Math.floor(Board.SIZE/2)+1, Math.floor(Board.SIZE/2)],
+      [Math.floor(Board.SIZE/2)+2, Math.floor(Board.SIZE/2)]],
+      board: this
+    };
+
+    this.snake = new SnakeGame.Snake(options);
+
+    this.apples = [];
+  };
+
+  Board.SIZE = 25;
+
+  Board.prototype.anyCollisions = function(coord){
+    var snakeFront = this.snake.segments[0];
+    var length = this.snake.segments.length;
+    var snakePenultimate = this.snake.segments[length-2];
+    var snakeEnd = this.snake.segments[length-1];
+    var diffX = snakeEnd[0] - snakePenultimate[0];
+    var diffY = snakeEnd[1] - snakePenultimate[1];
+    var otherSegments = this.snake.segments.slice(1);
+    var board = this;
+
+    this.apples.forEach(function (apple) {
+      if (snakeFront[0] === apple[0] && snakeFront[1] === apple[1]) {
+        var idx = board.apples.indexOf(apple);
+        board.apples.splice(idx, 1);
+        var newSegment = [snakeEnd[0] + diffX, snakeEnd[1] + diffY];
+        board.snake.segments.push(newSegment);
+      }
+    });
+
+    if (snakeFront[0] > Board.SIZE || snakeFront[1] > Board.SIZE ||
+        snakeFront[0] < 0 || snakeFront[1] < 0) {
+          console.log("You ran out of bounds!! " + JSON.stringify(snakeFront));
+          throw new SnakeError("You lost!!");
+    }
+
+    otherSegments.forEach(function(seg) {
+      if (seg[0] === snakeFront[0] && seg[1] === snakeFront[1]) {
+        throw new SnakeError("You hit yourself dummy!");
+      }
+    });
+
+  }
+
+  var SnakeError = SnakeGame.SnakeError = function(msg){
+    this.msg = msg;
+  }
+
+})();
